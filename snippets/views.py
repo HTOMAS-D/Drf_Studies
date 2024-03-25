1b2e2dd4938d664683cea71a77d36aebbc3f0a69
Ricky <henryats@gmail.com>
2024-03-25 12:48:16 +0000

    started user model

--- snippets/views.py
+++ snippets/views.py
@@ -1,13 +1,26 @@
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    
+    
+class UserList(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
